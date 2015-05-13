53f4b258bfea07031f091abb72e86202da5449dd
Ang Lee <angleedomain@gmail.com>
2015-05-13 21:30:03 +1000

    update q2

--- workshop8/q2.m
+++ workshop8/q2.m
@@ -1,4 +1,4 @@
-plot(Vgs, sqrt_Id, 'LineWidth', 5);
+plot(Vgs, sqrt_Id, 'LineWidth', 5, 'color', 'yellow');
 
 V_I_matrix = [Vgs sqrt_Id];
 V_I_matrix_selected = V_I_matrix(V_I_matrix(:, 1)>2, :);
@@ -11,6 +11,9 @@ y = p(1) * x + p(2);
 hold on;
 plot(x, y, 'Color',[1,0,0]);
 
-p(1) * p(1)
+legend('Original curve', 'Linear regression trend line');
+xlabel('Vgs (V)');
+ylabel('sqrt(Id) (A)');
 
+constant = p(1) * p(1)
 Vth = - p(2) / p(1)
