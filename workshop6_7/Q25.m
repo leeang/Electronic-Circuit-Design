11d0ca7e75607f49bcac59d5e7929d911ef86ab8
Ang Lee <angleedomain@gmail.com>
2015-05-18 17:16:15 +1000

    update lab67 plot

--- workshop6_7/Q25.m
+++ workshop6_7/Q25.m
@@ -2,10 +2,9 @@ bode1 = [frequency1 magnitude1];
 
 bode1_max = max(bode1(:, 2));
 
-semilogx([10 1E7], [bode1_max-3 bode1_max-3]);
-hold on;
-
 semilogx(bode1(:,1), bode1(:,2));
+hold on;
+semilogx([10 1E7], [bode1_max-3 bode1_max-3], ':');
 
 xlabel('Frequency');
 ylabel('Magnitude');
--- workshop6_7/Q31.m
+++ workshop6_7/Q31.m
@@ -6,16 +6,15 @@ bode2 = bode2(bode2(:, 2)<26.5, :);
 bode1_max = max(bode1(:, 2));
 bode2_max = max(bode2(:, 2));
 
-semilogx([10 1E7], [bode1_max-3 bode1_max-3]);
-hold on;
-
-semilogx([10 1E7], [bode2_max-3 bode2_max-3]);
-hold on;
-
 semilogx(bode1(:,1), bode1(:,2));
 hold on;
 semilogx(bode2(:,1), bode2(:,2));
 
+hold on;
+semilogx([10 1E7], [bode1_max-3 bode1_max-3], ':');
+hold on;
+semilogx([10 1E7], [bode2_max-3 bode2_max-3], ':');
+
 xlabel('Frequency');
 ylabel('Magnitude');
-legend('-3dB of Q25', '-3dB of Q31', 'Q25', 'Q31', 'location', 'southeast');
+legend('Q25', 'Q31', '-3dB of Q25', '-3dB of Q31', 'location', 'southeast');
